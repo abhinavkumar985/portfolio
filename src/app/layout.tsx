a346a752a478d158da90a0bab4d81aa95292c353
Abhinav Kumar <abhinavkumar985@gmail.com>
2025-05-12 16:02:12 +0200

    fix broken tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,11 +29,12 @@ export default function RootLayout({
     const handleOutboundLinkClick = (event) => {
       const link = event.target.closest('a');
       if (link && link.href && !link.href.includes(window.location.hostname)) {
+        const d = new URL(link.href);
         // @ts-expect-error type error type error
         gtag('event', 'click', {
           event_category: 'outbound',
           event_label: link.href,
-          event_value: link.textContent.trim(), // Add link text as event_value
+          event_value: link.textContent.trim() || d.hostname, // Add link text as event_value
           transport_type: 'beacon',
         });
       }
